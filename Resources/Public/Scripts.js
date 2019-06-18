c47c4c9c4c1c708f857968f9a293f2ba2dd355ac
Sebastian Helzle <sebastian@helzle.it>
2019-06-18 16:38:23 +0200

    FEATURE: Add cancel button to edit form

--- Resources/Public/Scripts.js
+++ Resources/Public/Scripts.js
@@ -2,23 +2,37 @@
     window.onload = function () {
         var redirectRows = document.querySelectorAll('.redirect-table__row');
         var redirectEditForm = document.getElementById('redirect-edit-form');
+        var activeEditRow = null;
+
+        redirectEditForm.querySelector('.add-redirect-form__cancel');
+        redirectEditForm.addEventListener('click', function(e) {
+            redirectEditForm.classList.remove('open');
+            activeEditRow = null;
+        });
 
         function setupRow(row) {
+            var rowId = row.dataset.redirectId;
             var redirectEditButton = row.querySelector('[data-edit-redirect-id]');
             var redirectProps = JSON.parse(row.dataset.redirectProps);
 
             redirectEditButton.addEventListener('click', function (e) {
-                row.parentNode.insertBefore(redirectEditForm, row.nextSibling);
-                redirectEditForm.querySelector('#edit-host').value = redirectProps.host || '';
-                redirectEditForm.querySelector('#edit-originalHost').value = redirectProps.host || '';
-                redirectEditForm.querySelector('#edit-sourceUriPath').value = redirectProps.sourceUriPath || '';
-                redirectEditForm.querySelector('#edit-originalSourceUriPath').value = redirectProps.sourceUriPath || '';
-                redirectEditForm.querySelector('#edit-targetUriPath').value = redirectProps.targetUriPath || '';
-                redirectEditForm.querySelector('#edit-statusCode').value = redirectProps.statusCode;
-                redirectEditForm.querySelector('#edit-comment').value = redirectProps.comment || '';
-                redirectEditForm.querySelector('#edit-startDateTime').value = redirectProps.startDateTime || '';
-                redirectEditForm.querySelector('#edit-endDateTime').value = redirectProps.endDateTime || '';
-                redirectEditForm.classList.add('open');
+                if (activeEditRow === rowId) {
+                    redirectEditForm.classList.remove('open');
+                    activeEditRow = null;
+                } else {
+                    activeEditRow = rowId;
+                    row.parentNode.insertBefore(redirectEditForm, row.nextSibling);
+                    redirectEditForm.querySelector('#edit-host').value = redirectProps.host || '';
+                    redirectEditForm.querySelector('#edit-originalHost').value = redirectProps.host || '';
+                    redirectEditForm.querySelector('#edit-sourceUriPath').value = redirectProps.sourceUriPath || '';
+                    redirectEditForm.querySelector('#edit-originalSourceUriPath').value = redirectProps.sourceUriPath || '';
+                    redirectEditForm.querySelector('#edit-targetUriPath').value = redirectProps.targetUriPath || '';
+                    redirectEditForm.querySelector('#edit-statusCode').value = redirectProps.statusCode;
+                    redirectEditForm.querySelector('#edit-comment').value = redirectProps.comment || '';
+                    redirectEditForm.querySelector('#edit-startDateTime').value = redirectProps.startDateTime || '';
+                    redirectEditForm.querySelector('#edit-endDateTime').value = redirectProps.endDateTime || '';
+                    redirectEditForm.classList.add('open');
+                }
             });
         }
 
