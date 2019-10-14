427adee6825b5c50ad57fe3124489901024e39cb
Sebastian Helzle <sebastian@helzle.it>
2019-10-14 15:54:47 +0200

    TASK: Replace double quotes with single

--- Classes/Controller/ModuleController.php
+++ Classes/Controller/ModuleController.php
@@ -408,12 +408,12 @@ class ModuleController extends AbstractModuleController
         $filename = 'neos-redirects-' . (new DateTime())->format('Y-m-d-H-i-s') . '.csv';
 
         $content = $csvWriter->getContent();
-        header("Pragma: no-cache");
-        header("Content-type: application/text");
-        header("Content-Length: " . strlen($content));
-        header("Content-Disposition: attachment; filename=" . $filename);
-        header("Content-Transfer-Encoding: binary");
-        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+        header('Pragma: no-cache');
+        header('Content-type: application/text');
+        header('Content-Length: ' . strlen($content));
+        header('Content-Disposition: attachment; filename=' . $filename);
+        header('Content-Transfer-Encoding: binary');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 
         echo $content;
 
